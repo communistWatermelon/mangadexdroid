99c4a34fe0a18e60fa590bb3d7101b88787afea3
communistWatermelon <zyko101@gmail.com>
2022-09-28 13:36:16 -0700

    Update chapter models

--- app/src/main/java/com/melonhead/mangadexfollower/models/content/Chapter.kt
+++ app/src/main/java/com/melonhead/mangadexfollower/models/content/Chapter.kt
@@ -1,8 +1,13 @@
 package com.melonhead.mangadexfollower.models.content
 
-@kotlinx.serialization.Serializable
-data class ChapterAttributes(val title: String?, val chapter: String?)
+import kotlinx.datetime.Instant
 
 @kotlinx.serialization.Serializable
-data class Chapter(val id: String, val attributes: ChapterAttributes)
+data class ChapterAttributes(val title: String?, val chapter: String?, val readableAt: Instant?)
+
+@kotlinx.serialization.Serializable
+data class ChapterRelationships(val id: String, val related: String? = null, val type: String? = null)
+
+@kotlinx.serialization.Serializable
+data class Chapter(val id: String, val attributes: ChapterAttributes, val relationships: List<ChapterRelationships>?)
 
