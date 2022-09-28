fcdfa0f93ec23c256b07645cebecc4533ea4e4a6
communistWatermelon <zyko101@gmail.com>
2022-09-28 00:08:06 -0700

    Create Koin AppModule

---
+++ app/src/main/java/com/melonhead/mangadexfollower/di/AppModule.kt
@@ -0,0 +1,44 @@
+package com.melonhead.mangadexfollower.di
+
+import com.melonhead.mangadexfollower.routes.*
+import com.melonhead.mangadexfollower.services.InMemoryTokenProvider
+import com.melonhead.mangadexfollower.services.TokenProviderService
+import com.melonhead.mangadexfollower.viewmodels.MainViewModel
+import io.ktor.client.*
+import io.ktor.client.engine.cio.*
+import io.ktor.client.plugins.contentnegotiation.*
+import io.ktor.serialization.kotlinx.json.*
+import kotlinx.serialization.json.Json
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.dsl.module
+
+val appModule = module {
+    single {
+        HttpClient(CIO) {
+            install(ContentNegotiation) {
+                json(Json {
+                    prettyPrint = true
+                    isLenient = true
+                    ignoreUnknownKeys = true
+                })
+            }
+        }
+    }
+
+    single<TokenProviderService> {
+        InMemoryTokenProvider()
+    }
+
+    single<LoginService> {
+        LoginServiceImpl(get(), get())
+    }
+
+    single<UserService> {
+        UserServiceImpl(get(), get())
+    }
+
+    single<MangaService> {
+        MangaServiceImpl(get())
+    }
+
+}
