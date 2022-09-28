ebdeeec605ac57cd6f69ab99033f4c7c2003a3ac
communistWatermelon <zyko101@gmail.com>
2022-09-28 13:37:12 -0700

    Fix manga service issues

--- app/src/main/java/com/melonhead/mangadexfollower/services/MangaService.kt
+++ app/src/main/java/com/melonhead/mangadexfollower/services/MangaService.kt
@@ -1,24 +1,30 @@
 package com.melonhead.mangadexfollower.services
 
+import com.melonhead.mangadexfollower.models.content.Manga
+import com.melonhead.mangadexfollower.models.content.MangaResponse
 import com.melonhead.mangadexfollower.routes.HttpRoutes.MANGA_URL
 import io.ktor.client.*
+import io.ktor.client.call.*
 import io.ktor.client.request.*
-import io.ktor.client.statement.*
 import io.ktor.http.*
 
 
 interface MangaService {
-    suspend fun getManga(): String
+    suspend fun getManga(id: String): Manga
 }
 
 class MangaServiceImpl(
     private val client: HttpClient,
 ): MangaService {
-    override suspend fun getManga(): String {
-        return client.get(MANGA_URL) {
+    override suspend fun getManga(id: String): Manga {
+        val result = client.get(MANGA_URL) {
             headers {
                 contentType(ContentType.Application.Json)
             }
-        }.bodyAsText()
+            url {
+                appendPathSegments(id)
+            }
+        }
+        return result.body<MangaResponse>().data
     }
 }
