9e837584c2a6279564658b10617bcc7ce0a57517
communistWatermelon <zyko101@gmail.com>
2022-09-28 23:43:11 -0700

    Add route for read markers

--- app/src/main/java/com/melonhead/mangadexfollower/routes/HttpRoutes.kt
+++ app/src/main/java/com/melonhead/mangadexfollower/routes/HttpRoutes.kt
@@ -5,13 +5,14 @@ object HttpRoutes {
 
     private const val AUTH_ROUTE = "${BASE_URL}/auth"
     private const val USER_ROUTE = "${BASE_URL}/user"
+    private const val MANGA_ROUTE = "${BASE_URL}/manga"
 
     const val LOGIN_URL = "${AUTH_ROUTE}/login"
     const val CHECK_TOKEN_URL = "${AUTH_ROUTE}/check"
     const val REFRESH_TOKEN_URL = "${AUTH_ROUTE}/refresh"
 
-    const val MANGA_URL = "${BASE_URL}/manga"
+    const val MANGA_URL = "${MANGA_ROUTE}/"
+    const val MANGA_READ_MARKERS_URL = "${MANGA_ROUTE}/{id}/read"
+
     const val USER_FOLLOW_CHAPTERS_URL = "${USER_ROUTE}/follows/manga/feed"
-
-
 }
