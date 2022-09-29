b868fd703b60dc9ceecf92b783c1b274ed8ef70a
communistWatermelon <zyko101@gmail.com>
2022-09-29 13:56:10 -0700

    Tweak error handling in manga and user service to prevent crashes

--- app/src/main/java/com/melonhead/mangadexfollower/services/MangaService.kt
+++ app/src/main/java/com/melonhead/mangadexfollower/services/MangaService.kt
@@ -1,5 +1,6 @@
 package com.melonhead.mangadexfollower.services
 
+import android.util.Log
 import com.melonhead.mangadexfollower.models.auth.AuthToken
 import com.melonhead.mangadexfollower.models.content.Manga
 import com.melonhead.mangadexfollower.models.content.MangaReadMarkersResponse
@@ -9,6 +10,7 @@ import com.melonhead.mangadexfollower.routes.HttpRoutes.MANGA_URL
 import io.ktor.client.*
 import io.ktor.client.call.*
 import io.ktor.client.request.*
+import io.ktor.client.statement.*
 import io.ktor.http.*
 
 
@@ -31,24 +33,33 @@ class MangaServiceImpl(
                 }
             }
         }
-        return result.body<MangaResponse>().data
+
+        return try {
+            result.body<MangaResponse>().data
+        } catch (e: Exception) {
+            Log.w("", "getManga: ${result.bodyAsText()}")
+            e.printStackTrace()
+            emptyList()
+        }
     }
 
     override suspend fun getReadChapters(mangaIds: List<String>, token: AuthToken): List<String> {
-        return try {
-            val result = client.get(MANGA_READ_MARKERS_URL) {
-                headers {
-                    contentType(ContentType.Application.Json)
-                    bearerAuth(token.session)
-                }
-                url {
-                    mangaIds.forEach {
-                        encodedParameters.append("ids[]", it)
-                    }
+        val result = client.get(MANGA_READ_MARKERS_URL) {
+            headers {
+                contentType(ContentType.Application.Json)
+                bearerAuth(token.session)
+            }
+            url {
+                mangaIds.forEach {
+                    encodedParameters.append("ids[]", it)
                 }
             }
+        }
+
+        return try {
             result.body<MangaReadMarkersResponse>().data
         } catch (e: Exception) {
+            Log.w("", "getReadChapters: ${result.bodyAsText()}")
             e.printStackTrace()
             emptyList()
         }
--- app/src/main/java/com/melonhead/mangadexfollower/services/UserService.kt
+++ app/src/main/java/com/melonhead/mangadexfollower/services/UserService.kt
@@ -1,5 +1,6 @@
 package com.melonhead.mangadexfollower.services
 
+import android.util.Log
 import com.melonhead.mangadexfollower.models.auth.AuthToken
 import com.melonhead.mangadexfollower.models.auth.PaginatedResponse
 import com.melonhead.mangadexfollower.models.content.Chapter
@@ -7,6 +8,7 @@ import com.melonhead.mangadexfollower.routes.HttpRoutes
 import io.ktor.client.*
 import io.ktor.client.call.*
 import io.ktor.client.request.*
+import io.ktor.client.statement.*
 import io.ktor.http.*
 import java.time.ZoneId
 import java.time.format.DateTimeFormatter
@@ -20,26 +22,28 @@ class UserServiceImpl(
     private val client: HttpClient
 ): UserService {
     override suspend fun getFollowedChapters(token: AuthToken, createdAtSince: Long): PaginatedResponse<List<Chapter>> {
+        val result = client.get(HttpRoutes.USER_FOLLOW_CHAPTERS_URL) {
+            headers {
+                contentType(ContentType.Application.Json)
+                bearerAuth(token.session)
+            }
+            url {
+                encodedParameters.append("translatedLanguage[]", "en")
+                parameters.append("order[createdAt]", "desc")
+                parameters.append("limit", "50")
+                if (createdAtSince != 0L) {
+                    val date = Date()
+                    date.time = createdAtSince
+                    val utcDate = date.toInstant().atZone(ZoneId.of("UTC"))
+                    val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'kk:mm:ss")
+                    encodedParameters.append("createdAtSince", formatter.format(utcDate))
+                }
+            }
+        }
         return try {
-            client.get(HttpRoutes.USER_FOLLOW_CHAPTERS_URL) {
-                headers {
-                    contentType(ContentType.Application.Json)
-                    bearerAuth(token.session)
-                }
-                url {
-                    encodedParameters.append("translatedLanguage[]", "en")
-                    parameters.append("order[createdAt]", "desc")
-                    parameters.append("limit", "50")
-                    if (createdAtSince != 0L) {
-                        val date = Date()
-                        date.time = createdAtSince
-                        val utcDate = date.toInstant().atZone(ZoneId.of("UTC"))
-                        val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'kk:mm:ss")
-                        encodedParameters.append("createdAtSince", formatter.format(utcDate))
-                    }
-                }
-            }.body()
+            result.body()
         } catch (e: Exception) {
+            Log.w("", "getFollowedChapters: ${result.bodyAsText()}")
             e.printStackTrace()
             PaginatedResponse(0, 0, 0, listOf())
         }
