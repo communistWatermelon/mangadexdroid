620a898b6369c87165f3590e5605e217831a78c2
communistWatermelon <zyko101@gmail.com>
2022-09-29 22:34:57 -0700

    Add manga cover route

--- app/src/main/java/com/melonhead/mangadexfollower/routes/HttpRoutes.kt
+++ app/src/main/java/com/melonhead/mangadexfollower/routes/HttpRoutes.kt
@@ -6,6 +6,7 @@ object HttpRoutes {
     private const val AUTH_ROUTE = "${BASE_URL}/auth"
     private const val USER_ROUTE = "${BASE_URL}/user"
     private const val MANGA_ROUTE = "${BASE_URL}/manga"
+    private const val COVER_ROUTE = "${BASE_URL}/cover"
 
     const val LOGIN_URL = "${AUTH_ROUTE}/login"
     const val CHECK_TOKEN_URL = "${AUTH_ROUTE}/check"
@@ -15,4 +16,5 @@ object HttpRoutes {
     const val MANGA_READ_MARKERS_URL = "${MANGA_ROUTE}/read"
 
     const val USER_FOLLOW_CHAPTERS_URL = "${USER_ROUTE}/follows/manga/feed"
+    const val COVER_URL = "${COVER_ROUTE}/"
 }
