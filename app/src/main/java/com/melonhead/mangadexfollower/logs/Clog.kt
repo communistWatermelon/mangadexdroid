996986d77c1efaee66ff0077069d1f7b71846799
communistWatermelon <zyko101@gmail.com>
2022-10-12 11:44:59 -0700

    Automatically check authentication on 401 errors with "unauthorized_http_exception": "User not found"

--- app/src/main/java/com/melonhead/mangadexfollower/logs/Clog.kt
+++ app/src/main/java/com/melonhead/mangadexfollower/logs/Clog.kt
@@ -3,28 +3,37 @@ package com.melonhead.mangadexfollower.logs
 import android.util.Log
 import com.google.firebase.crashlytics.ktx.crashlytics
 import com.google.firebase.ktx.Firebase
+import com.melonhead.mangadexfollower.repositories.AuthRepository
 import io.ktor.client.network.sockets.*
+import org.koin.core.component.KoinComponent
+import org.koin.core.component.inject
 
-object Clog {
-    inline fun d(message: String) {
+object Clog: KoinComponent {
+    private val authRepository: AuthRepository by inject()
+
+    fun d(message: String) {
         Log.d(null, message)
         Firebase.crashlytics.log(message)
     }
 
-    inline fun i(message: String) {
+    fun i(message: String) {
         Log.i(null, message)
         Firebase.crashlytics.log(message)
     }
     
-    inline fun w(message: String) {
+    fun w(message: String) {
         Log.w(null, message)
         Firebase.crashlytics.log(message)
     }
 
-    inline fun e(message: String, exception: Exception) {
+    suspend fun e(message: String, exception: Exception) {
         i(message)
+        if (message.contains("User not found", ignoreCase = true) && message.contains("unauthorized_http_exception", ignoreCase = true)) {
+            authRepository.checkCurrentAuthentication()
+            return
+        }
         when (exception) {
-             is ConnectTimeoutException -> return
+            is ConnectTimeoutException -> return
         }
         Firebase.crashlytics.recordException(exception)
     }
--- app/src/main/java/com/melonhead/mangadexfollower/repositories/AuthRepository.kt
+++ app/src/main/java/com/melonhead/mangadexfollower/repositories/AuthRepository.kt
@@ -1,14 +1,12 @@
 package com.melonhead.mangadexfollower.repositories
 
+import com.melonhead.mangadexfollower.logs.Clog
 import com.melonhead.mangadexfollower.models.auth.AuthToken
-import com.melonhead.mangadexfollower.services.LoginService
-import com.melonhead.mangadexfollower.services.AppDataService
 import com.melonhead.mangadexfollower.models.ui.LoginStatus
+import com.melonhead.mangadexfollower.services.AppDataService
+import com.melonhead.mangadexfollower.services.LoginService
 import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.SharingStarted
-import kotlinx.coroutines.flow.collectLatest
-import kotlinx.coroutines.flow.shareIn
+import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
 
 class AuthRepository(
@@ -28,8 +26,13 @@ class AuthRepository(
             }
         }
     }
+    suspend fun checkCurrentAuthentication() {
+        val token = appDataService.token.firstOrNull()
+        checkAuthentication(token)
+    }
 
     private suspend fun checkAuthentication(token: AuthToken?) {
+        Clog.i("checkAuthentication")
         mutableIsLoggedIn.value = LoginStatus.LoggingIn
         var currentToken: AuthToken? = token
         if (currentToken == null) {
@@ -39,6 +42,7 @@ class AuthRepository(
         }
         val validToken = loginService.isTokenValid(currentToken)
         if (!validToken) {
+            Clog.i("Token isn't valid, refreshing")
             currentToken = loginService.refreshToken(currentToken)
         }
         appDataService.updateToken(currentToken)
@@ -46,6 +50,7 @@ class AuthRepository(
     }
 
     suspend fun authenticate(email: String, password: String) {
+        Clog.i("authenticate")
         val token = loginService.authenticate(email, password)
         checkAuthentication(token)
     }
