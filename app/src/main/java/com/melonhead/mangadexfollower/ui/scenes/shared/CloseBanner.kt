614a839b6f66b16cf3f26bb5e21030b6c9a94d97
communistWatermelon <zyko101@gmail.com>
2024-07-17 14:48:13 -0700

    Fix incorrect colour for done button

--- app/src/main/java/com/melonhead/mangadexfollower/ui/scenes/shared/CloseBanner.kt
+++ app/src/main/java/com/melonhead/mangadexfollower/ui/scenes/shared/CloseBanner.kt
@@ -3,11 +3,7 @@ package com.melonhead.mangadexfollower.ui.scenes.shared
 import android.content.res.Configuration
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Check
 import androidx.compose.material.icons.filled.Close
@@ -15,6 +11,7 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.tooling.preview.Preview
@@ -42,6 +39,7 @@ fun CloseBanner(
                 Image(
                     imageVector = Icons.Default.Check,
                     contentDescription = "Close Button",
+                    colorFilter = ColorFilter.tint(MaterialTheme.colorScheme.surface)
                 )
             }
         }
